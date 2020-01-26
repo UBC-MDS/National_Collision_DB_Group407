2772ffd2c5623ce242d17142144a55bb83f20ff8
Sam Chepal <c.samneet@gmail.com>
2020-01-25 17:31:35 -0800

    added usage and fixed dta_read.R file

--- src/data_read.R
+++ src/data_read.R
@@ -1,13 +1,13 @@
 # author: DSCI_522_Group_407
 # date: 2020-01-22
 #
-"This script is used to download data (.csv file) from a 
-publicly accessible URL. 
- 
+"This script is used to download data (.csv file) from a
+publicly accessible URL.
+
 Usage: data_read.R --filepath=<filepath>
- 
+
 Example:
-   Rscript data_read.R --filepath='https://opendatatc.blob.core.windows.net/opendatatc/NCDB_2017.csv'
+   Rscript src/data_read.R --filepath='https://opendatatc.blob.core.windows.net/opendatatc/NCDB_2017.csv'
 
 Options:
 <filepath>        Takes a file path (this is a required positional argument)
@@ -26,22 +26,23 @@ opt <- docopt(doc)
 #' @return a raw_data.csv file saved in the data folder
 #' @examples
 #' main('https://file.csv')
+
 main <- function(file_path) {
     if (str_sub(file_path, start = -3) == 'csv') {
-    download.file(url = opt$filepath, destfile = "../data/raw_data.csv")
-    data <- read.csv("../data/raw_data.csv")
+    download.file(url = opt$filepath, destfile = "data/raw_data.csv")
+    data <- read.csv("data/raw_data.csv")
     } else {
     print("the url type shoule be .csv file")
     }
 }
- 
+
 test_main <- function(){
   test_that("The file type should be .csv", {
       expect_match(
           main('https://opendatatc.blob.core.windows.net/opendatatc/NCDB_2017.html'), "the url type shoule be .csv file")
   })
 }
-            
+
 test_main()
-    
+
 main(opt$filepath)
